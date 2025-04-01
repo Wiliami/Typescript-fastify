4413393d4c992ca283b361994fc0116fa0ca5a20
wiliami <wiliaup97@gmail.com>
2025-04-01 17:45:44 -0400

    feat: new feature

---
+++ src/routes/users/create.ts
@@ -0,0 +1,16 @@
+interface User {
+    name: string    
+    email: string
+    password: string
+}
+
+function createUser(data: User) {
+
+    console.log(data)
+}
+
+createUser({
+    name: 'Wiliamis',
+    email: 'oliveira@gmail.com',
+    password: '123'
+})
